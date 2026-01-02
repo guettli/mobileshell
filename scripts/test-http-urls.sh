934c658c14d1553017b9bf22844eb3f2e9c92c32
Thomas Guettler <info@thomas-guettler.de>
2026-01-02 19:17:52 +0100

    ...

--- scripts/test-http-urls.sh
+++ scripts/test-http-urls.sh
@@ -10,7 +10,7 @@ if [[ -z "${IN_NIX_SHELL:-}" ]]; then
 fi
 
 # shellcheck disable=SC2046
-http_locations=$(rg -n 'https?://' $(git ls-files | grep -vP '\.md$' | grep -vP 'internal/server/static|scripts/test-jsdom|jsdom-test.mjs') | { grep -vP 'github.com/guettli/bash-strict-mode|http://%s|Found string|example.com' || true; })
+http_locations=$(rg -n 'https?://' $(git ls-files | grep -vP '\.md$' | grep -vP 'internal/server/static|scripts/test-jsdom|jsdom.*.mjs') | { grep -vP 'github.com/guettli/bash-strict-mode|http://%s|Found string|example.com' || true; })
 if [[ -n $http_locations ]]; then
     echo "Found string 'https://' in code. This should be avoided. All needed files should be embeded into the binary via go:embed"
     echo
